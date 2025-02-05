81de776b06bc625a9814fb0f462ca6830f0bd65e
Bryan Paredes <a21311054@uthermosillo.edu.mx>
2025-02-04 19:45:05 -0700

    Crea Store de pinia para almacenar los datos de registro

--- src/modules/registro/views/RegistrarView.vue
+++ src/modules/registro/views/RegistrarView.vue
@@ -1,19 +1,19 @@
 <template>
 <div>
     <h2>Formulario de registro</h2>
-    <Form :validation-schema="schema" @submit="">
+    <Form :validation-schema="schema" @submit="onSubmit">
         <div class="from">
         <label for="nombre">Nombre</label>
-    <Field type="text" name="nombre" id="nombre"placeholder="ingrese su nombre" />
+    <Field v-model="nombre" type="text" name="nombre" id="nombre"placeholder="ingrese su nombre" />
     <ErrorMessage name="nombre"></ErrorMessage>
         </div>
         <div class="from">
     <label for="nombre">Correo</label>
-    <Field type="email" name="email" id="correo" placeholder="ingrese su email"/>
+    <Field v-model="email" type="email" name="email" id="correo" placeholder="ingrese su email"/>
     <ErrorMessage name="email"></ErrorMessage>
         </div>
    <div class="from">
-    <button type="submit">Registrar</button>]
+    <button type="submit">Registrar</button>
    </div>
 </Form>
 </div> 
@@ -22,9 +22,17 @@
 <script setup>
 import {Form, Field, ErrorMessage} from 'vee-validate';
 import { schema } from '../schemas/validationSchema';
+import {useRegistrarStore} from "../stores/registrarStore";
+import { ref } from 'vue';
+
+const registarStore = useRegistrarStore();
+
+const nombre = ref('');
+const email = ref('');
 
 const onSubmit = () =>{
-    console.log('Se ha enviado el formulario')
+    registarStore.guardarRegistro(nombre.value, email.value);
+    console.log('Se ha enviado el formulario');
 }
 </script>
 
