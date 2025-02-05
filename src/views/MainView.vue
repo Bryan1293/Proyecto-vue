5c0c48276d74b40f4b81c9d784a121237614a3d7
Bryan Paredes <a21311054@uthermosillo.edu.mx>
2025-02-04 18:49:40 -0700

    Crea componente lista de tareas, borra componentes autogenerados, crea rutas

---
+++ src/views/MainView.vue
@@ -0,0 +1,25 @@
+import Contador from '/';
+<template>
+<div class="container">
+    <Sidebar></Sidebar>
+    <div class="content">
+<router-view></router-view>
+    </div>
+</div>
+
+</template>
+
+<script setup>
+import Sidebar from '@/components/Sidebar.vue';
+import { routeLocationKey } from 'vue-router';
+</script>
+
+<style>
+.container {
+    display: flex;
+}
+.content{
+    flex: 1;
+    padding: 20%;
+}
+</style>
